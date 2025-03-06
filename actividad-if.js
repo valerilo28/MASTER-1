48e6f5f48dd31cea7e6ffec49a04ace0169005bc
valerilo28 <valeria.rivero.lopez@gmail.com>
2025-03-06 00:32:28 -0600

    ACTIVIDAD IF

---
+++ actividad-if.js
@@ -0,0 +1,21 @@
+
+let nota = 98;
+
+if(nota >= 90){
+    console.log("EXCELENTE");
+}
+else if(nota >= 75 && nota <= 89){
+    console.log("BIEN");
+}
+else if(nota >= 60 && nota <= 74){
+    console.log("SUFICIENTE")
+}
+else{
+    console.log("NO APROBADO");
+}
+
+
+let suma = 0; let numero = 1; 
+while (suma < 5) 
+    { suma += numero; numero++; }
+console.log(suma);
