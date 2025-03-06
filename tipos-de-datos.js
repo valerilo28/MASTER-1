a10c1dec2ff2dbd0a2aed06ed069aa50a8db246d
valerilo28 <valeria.rivero.lopez@gmail.com>
2025-03-06 00:26:19 -0600

    ACTIVIDAD TIPOS DE DATOS

---
+++ tipos-de-datos.js
@@ -0,0 +1,31 @@
+
+// Imprimir en la consola los distintos tipos de datos que existen en JS
+
+console.log(typeof 42);
+
+console.log(typeof "Valeria");
+
+console.log(typeof true);
+
+console.log(typeof 'Veinticinco');
+
+console.log(typeof -666);
+
+console.log(typeof 0);
+
+console.log(typeof "");
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof FALSE);
+
+console.log(typeof 28.05);
+
+console.log(typeof p);
+
+let l = 1289327*947832;
+
+console.log(typeof l);
+
